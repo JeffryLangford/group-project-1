89e3e9f88ed0fde1c890e764ab71e1e830690a87
CMiller <mlr.chase40@gmail.com>
2022-05-02 20:16:07 -0500

    code cleanup"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,30 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+//const { stringify } = require("postcss");
 
-var drinkTarget = document.getElementById("drinks");
-var entertainmentTarget = document.getElementById("entertainment");
-var activeTarget = document.getElementById("active");
-var foodTarget = document.getElementById("food");
-var atmosphereTarget = document.getElementById("atmosphere");
-var parkingTarget = document.getElementById("parking");
+//variables needed for weather
+var slider = document.getElementById("mile-range");
+var milesValue = document.getElementById("miles-chosen");
+var addressValue = document.getElementById("address-input")
 
-output.innerHTML = slider.value; // Display the default slider value
+//variables needed for fourqsuare
+var drinkEl = document.getElementById("drinks");
+var entertainmentEl = document.getElementById("entertainment");
+var activeEl = document.getElementById("active");
+var foodEl = document.getElementById("food");
+var atmosphereEl = document.getElementById("atmosphere");
+var parkingEl = document.getElementById("parking");
 
-// FOURSQUARE API REQUEST
-// foursquare Places API key fsq3Dd7JeFQQHyDysLsuKKzrNfbaWgHDH09HMsub7/9FfFA=
+var weatherEl = document.getElementById("weather-results");
+var locationEl = document.getElementById("location-results");
+var popularEl = document.getElementById("popular-events");
 
+//variables needed for functions and functionalty
+var wrapEl = document.getElementById("wrap");
+var sectionEl = document.getElementById("section");
+var errorEl = document.getElementById("error");
+var submitButton = document.getElementById("submit-button");
+
+// FOURSQUARE API
+// foursquare authourization key: 'fsq3Dd7JeFQQHyDysLsuKKzrNfbaWgHDH09HMsub7/9FfFA='
 
 const options = {
   method: 'GET',
@@ -22,49 +34,92 @@ const options = {
   }
 };
 
-fetch('https://api.foursquare.com/v3/places/search?near=\'Austin, TX\'', options).then(response => {
+fetch('https://api.foursquare.com/v3/places/search?near=\'Austin, TX\'&categories=10000&sort=rating', options).then(response => {
   if (response.ok) {
       response.json().then(data => {
-          console.log(data);
+        
+          console.log(data['results']);
       });
   } else {
       alert("ERROR: NOT WORKING");
   }
 }).catch(err => console.error(err));
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-drinkTarget.addEventListener('click', function () {
+//when clicking the different categories, show more specific results
+drinkEl.addEventListener('click', function () {
   var drinkResults = document.getElementById("drink-results");
+  if (drinkResults.classList.contains("hide")) {
   drinkResults.classList.remove("hide");
+  } else{
+    drinkResults.classList.add("hide");
+  }
   console.log("clicked drinks");
 });
-entertainmentTarget.addEventListener('click', function () {
+entertainmentEl.addEventListener('click', function () {
   var entertainmentResults = document.getElementById("entertainment-results");
+  if (entertainmentResults.classList.contains("hide")) {
   entertainmentResults.classList.remove("hide");
+  } else{
+    entertainmentResults.classList.add("hide");
+  }
   console.log("clicked entertainment");
 });
-activeTarget.addEventListener('click', function () {
+activeEl.addEventListener('click', function () {
   var activeResults = document.getElementById("active-results");
+  if (activeResults.classList.contains("hide")) {
   activeResults.classList.remove("hide");
+  } else{
+    activeResults.classList.add("hide");
+  }
   console.log("clicked active");
 });
-foodTarget.addEventListener('click', function () {
+foodEl.addEventListener('click', function () {
   var foodResults = document.getElementById("food-results");
+  if (foodResults.classList.contains("hide")) {
   foodResults.classList.remove("hide");
+  } else{
+    foodResults.classList.add("hide");
+  }
   console.log("clicked food");
 });
-atmosphereTarget.addEventListener('click', function () {
+atmosphereEl.addEventListener('click', function () {
   var atmosphereResults = document.getElementById("atmosphere-results");
+  if (atmosphereResults.classList.contains("hide")) {
   atmosphereResults.classList.remove("hide");
+  } else{
+    atmosphereResults.classList.add("hide");
+  }
   console.log("clicked atmosphere");
 });
-parkingTarget.addEventListener('click', function () {
+parkingEl.addEventListener('click', function () {
   var parkingResults = document.getElementById("parking-results");
+  if (parkingResults.classList.contains("hide")) {
   parkingResults.classList.remove("hide");
+  } else{
+    parkingResults.classList.add("hide");
+  }
   console.log("clicked parking");
 });
 
+
+//testing to store miles and range
+function submitResults () {
+  console.log(milesValue.innerHTML);
+  console.log(addressValue.value);
+  //var finalMiles = milesValue.innerHTML;
+  //var finalAddress = addressValue.value;
+  if (!addressValue.value) {
+    errorEl.classList.remove("hide");
+    return;
+  }
+  wrapEl.classList.add("hide");
+  sectionEl.classList.add("hide");
+  errorEl.classList.add("hide");
+  submitButton.classList.add("hide");
+  weatherEl.classList.remove("hide");
+  locationEl.classList.remove("hide");
+  popularEl.classList.remove("hide");
+}
+ 
+//event listeners on buttons
+submitButton.addEventListener('click', submitResults);
