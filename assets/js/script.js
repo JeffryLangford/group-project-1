52bcd6d63073fcfabfd90321347b86dcaf1ba9e1
CMiller <mlr.chase40@gmail.com>
2022-05-02 22:15:17 -0500

    code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,26 +70,6 @@ var errorEl = document.getElementById("error");
 var submitButton = document.getElementById("submit-button");
 var goBackButton = document.getElementById("go-back-button");
 
-// FOURSQUARE API REQUEST
-// foursquare Places API key fsq3Dd7JeFQQHyDysLsuKKzrNfbaWgHDH09HMsub7/9FfFA=
-const options = {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    Authorization: 'fsq3Dd7JeFQQHyDysLsuKKzrNfbaWgHDH09HMsub7/9FfFA='
-  }
-};
-
-fetch('https://api.foursquare.com/v3/places/search?near=\'Austin, TX\'', options).then(response => {
-  if (response.ok) {
-      response.json().then(data => {
-          console.log(data);
-      });
-  } else {
-      alert("ERROR: NOT WORKING");
-  }
-}).catch(err => console.error(err));
-
 //when clicking the different categories, show more specific results
 drinkEl.addEventListener('click', function () {
   var drinkResults = document.getElementById("drink-results");
